709b1f296299c0cbf8f0292c8f87aedac96d4345
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2025-05-30 07:31:03 +0300

    Update id.js

--- cmds/id.js
+++ cmds/id.js
@@ -1,55 +1,27 @@
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-
 module.exports = {
     name: "id",
     usePrefix: false,
     usage: "id [@mention]",
-    version: "1.1",
-    description: "Fetch the Facebook User ID (UID) of a mentioned user or yourself and show their profile picture.",
+    version: "1.0",
+    description: "Fetch the Facebook User ID (UID) of a mentioned user or yourself.",
     admin: false,
     cooldown: 10,
 
     execute: async ({ api, event }) => {
         const { threadID, messageID, senderID, mentions } = event;
-
+        
         let uid;
         let userName;
 
+        // Check if a user is mentioned
         if (Object.keys(mentions).length > 0) {
-            uid = Object.keys(mentions)[0];
-            userName = mentions[uid].replace("@", "");
+            uid = Object.keys(mentions)[0]; // Get the first mentioned user's ID
+            userName = mentions[uid].replace("@", ""); // Get mentioned user's name
         } else {
-            uid = senderID;
+            uid = senderID; // If no mention, get the sender's UID
             userName = "You";
         }
-
-        const imageUrl = `https://graph.facebook.com/${uid}/picture?width=512&height=512`;
-        const imagePath = path.join(__dirname, `tmp_${uid}.jpg`);
-
-        // Download the profile picture
-        const file = fs.createWriteStream(imagePath);
-        https.get(imageUrl, (response) => {
-            response.pipe(file);
-            file.on("finish", () => {
-                file.close(() => {
-                    api.sendMessage(
-                        {
-                            body: `🔍 Facebook UID for ${userName}: ${uid}`,
-                            attachment: fs.createReadStream(imagePath),
-                        },
-                        threadID,
-                        () => {
-                            fs.unlinkSync(imagePath); // Clean up after sending
-                        },
-                        messageID
-                    );
-                });
-            });
-        }).on("error", (err) => {
-            fs.unlinkSync(imagePath);
-            api.sendMessage(`❌ Failed to fetch profile picture. UID: ${uid}`, threadID, messageID);
-        });
-    },
+        
+        api.sendMessage(`🔍 Facebook UID for ${userName}: ${uid}`, threadID, messageID);
+    }
 };
